7c199bd70a58f7ec1f6f01c30db44bf97aca2d94
Dave McCoy <cospan@gmail.com>
2011-07-05 19:34:09 -0400

    mg_defines holds all the defines that are global to the project

---
+++ miracle_grow/rtl/mg_defines.v
@@ -0,0 +1,18 @@
+//mg_defines.v
+
+// defines for the miracle grow project
+
+`ifndef
+`define __MG_DEFINES__
+
+`define COMMAND_PING 		32'h00000000
+`define COMMAND_WRITE 		32'h00000001
+`define COMMAND_READ		32'h00000002
+`define COMMAND_WSTREAM_C 	32'h00000003
+`define COMMAND_WSTREAM		32'h00000004
+`define COMMAND_RSTREAM_C	32'h00000005
+`define COMMAND_RSTREAM		32'h00000006
+`define COMMAND_RW_FLAGS	32'h00000007
+`define COMMAND_INTERRUPTS	32'h00000008
+
+`endif
