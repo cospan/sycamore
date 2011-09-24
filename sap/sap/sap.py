e6a0c0fabc819de1b9847c3ac8776e03000279cf
Dave McCoy <cospan@gmail.com>
2011-09-24 09:15:43 -0400

    reorganizing sap to a more unified model

---
+++ sap/sap/sap.py
@@ -0,0 +1,13 @@
+# Sap main interface
+
+
+def test():
+	""" Test Everythin in all subcomponents"""
+
+
+def test_saplib():
+	"""Test all saplib components"""
+
+
+if __name__ == "__main__":
+	print "hi"
