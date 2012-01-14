b3aab27152aa6e6be8309bc794b2be063f22a558
Dave McCoy <cospan@gmail.com>
2012-01-14 00:47:33 -0500

    Modifed the tty gpio test driver to read from user and write to user

--- xylem/driver/sycamore/drivers/gpio/gpio_driver.c
+++ xylem/driver/sycamore/drivers/gpio/gpio_driver.c
@@ -1,6 +1,7 @@
 
 #include <linux/kernel.h>
 #include <linux/tty.h>
+#include <linux/tty_flip.h>
 #include <linux/tty_driver.h>
 #include <linux/platform_device.h>
 #include "../../devices/devices.h"
@@ -10,18 +11,19 @@
 
 #define SYCAMORE_TTY_MAJOR 240
 
+#define BUFFER_SIZE 100
 
 typedef struct _sycamore_tty_driver_t sycamore_tty_driver_t;
 
 static int sycamore_platform_probe(struct platform_device *pdev);
 static int __devexit sycamore_platform_remove(struct platform_device *pdev);
 
-static int sycamore_tty_open(struct tty_driver * driver, struct file * filep);
-static void sycamore_tty_close(struct tty_driver * driver, struct file * filep);
+static int sycamore_tty_open(struct tty_struct * tty, struct file * filep);
+static void sycamore_tty_close(struct tty_struct * tty, struct file * filep);
 static int sycamore_tty_write(struct tty_struct * tty, 
 								const unsigned char *buf, int count);
 static int sycamore_tty_write_room(struct tty_struct * tty);
-static int sycamore_tty_set_termios(struct tty_struct *tty, struct ktermios * old);
+static void sycamore_tty_set_termios(struct tty_struct *tty, struct ktermios * old);
 
 static struct platform_driver sycamore_platform_driver = {
 	.probe	= sycamore_platform_probe,
@@ -34,8 +36,11 @@ static struct platform_driver sycamore_platform_driver = {
 };
 
 struct _sycamore_tty_driver_t {
+	struct platform_device *pdev;
 	struct tty_driver *tty_driver;
+	struct tty_struct *tty;
 	int lock;
+	char buffer[BUFFER_SIZE];
 };
 
 static sycamore_tty_driver_t sycamore_tty_driver;
@@ -55,7 +60,7 @@ static int __init sycamore_tty_init(void){
 	result = platform_driver_register(&sycamore_platform_driver);	
 	printk (KERN_INFO "%s: registering platform", __func__);
 	if (result != 0){
-		printk (KERN_INFO "%s: Failed to register platform device", __func__);
+		printk (KERN_INFO "%s: Failed to register platform driver", __func__);
 		return result;
 	}
 	sycamore_tty_driver.lock = 0;
@@ -92,8 +97,9 @@ static int sycamore_platform_probe(struct platform_device *pdev){
 
 	sycamore_tty_driver.tty_driver->init_termios = tty_std_termios;
 	sycamore_tty_driver.tty_driver->init_termios.c_cflag = B9600 | CS8 | CREAD | HUPCL | CLOCAL;
-	//tty_set_operations(&sycamore_tty_driver.tty_driver, &sycamore_tty_ops);
+	tty_set_operations(sycamore_tty_driver.tty_driver, &sycamore_tty_ops);
 	
+	sycamore_tty_driver.pdev = pdev;
 
 	//register this tty_device
 	//this line registers the first minor number too!, so if you only want one
@@ -119,21 +125,47 @@ static int __devexit sycamore_platform_remove(struct platform_device *pdev){
 
 
 //tty specific operations
-static int sycamore_tty_open(struct tty_driver * driver, struct file * filep){
+static int sycamore_tty_open(struct tty_struct * tty, struct file * filep){
 	return 0;
 }
-static void sycamore_tty_close(struct tty_driver * driver, struct file * filep){
+static void sycamore_tty_close(struct tty_struct * tty, struct file * filep){
 }
 static int sycamore_tty_write(struct tty_struct * tty, 
 								const unsigned char *buf, int count){
-	printk (KERN_INFO "%s: user entered: %s", __func__, buf);
+	int i = 0;
+	char * buffer = NULL;
+
+	
+	buffer = &sycamore_tty_driver.buffer[0];
+	//BUFFER_SIZE
+//	printk ("%s (sycamore driver)\n", __func__);
+//	printk ("\tbuf:\t\t%p\n", &buf);
+//	printk ("\tcount:\t\t%d\n", count);
+//	printk ("\tbuffer_size:\t%d\n", BUFFER_SIZE);
+
+	for (i = 0; i < count && i < BUFFER_SIZE; i++){
+//		printk("buf + i: %c\n", buf[i]);
+		buffer[i] = buf[i];
+//		get_user(buffer[i], buf + i);
+		
+//		printk("buffer: %c\n", buffer[i]);
+	}
+	buffer[count] = 0;
+
+	printk (KERN_INFO "%s: user entered: %s\n", __func__, buffer);
+
+	if (buffer[0] == 'h'){
+		printk("user_string\n");
+		tty_buffer_request_room(tty, count);
+		tty_insert_flip_string(tty, "read!!\r\n", 8);
+		tty_flip_buffer_push(tty);
+	}
 	return count;
 }
 static int sycamore_tty_write_room(struct tty_struct * tty){
-	return 10;
+	return BUFFER_SIZE;
 }
-static int sycamore_tty_set_termios(struct tty_struct *tty, struct ktermios * old){
-	return 0;
+static void sycamore_tty_set_termios(struct tty_struct *tty, struct ktermios * old){
 }
 
 module_init(sycamore_tty_init);
